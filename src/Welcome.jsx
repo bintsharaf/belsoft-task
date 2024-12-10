d9b200c0eb272c0a919f58c512415d38580d9d78
bintsharaf <138716264+bintsharaf@users.noreply.github.com>
2024-12-10 16:28:51 +0100

    Navigation bar
    Added google font to the project and wrapped the nav bar into a div

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import "./Welcome.css"
 export default function(Welcome) {
     return (<div className="Welcome">
-        <nav>
-            
-        </nav>
-        Belsoft
-    </div>);
+        <div>
+            <nav>
+            <   img src="/" alt="founderLogo" />
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">About Us</a></li>
+                    <li><a href="/">Gallary</a></li>
+                </ul>
+                <a href="/" className="btn">Register </a>
+            </nav>
+       </div>
+       <div className="firstPage">
+            <div className="row">
+                <div className="col-6">
+                    <h5></h5>
+
+                </div>
+            </div>
+       </div>
+    </div>
+    );
 }
