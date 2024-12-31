9dab2f85e8322b9b9202f9ee7ae041737da1adc4
bintsharaf <138716264+bintsharaf@users.noreply.github.com>
2024-12-31 23:32:36 +0100

    Successfully made the first Page responsive

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -11,41 +11,46 @@ export default function(Welcome) {
     return (<div className="Welcome">
         
         <div className="body" >
-        
+        <Header />
        <div className="firstPage">
         
-            <div className="row">
+            <div className="row firstP-section">
                 
-                <div className="col-md-12 mt-4 firstP-section">
-                    <Header />
+                <div className="col mt-3 ">
+                    
                     <div>
                    <h5>Join our premier monthly meetup for startup founders and tech visionaries</h5>
                     <h2 >Connect, Collaborate, Innovate!</h2>
-                    <p className="mb-5">Every last Friday of the month, we bring together the brightest minds in our local tech ecosystem. Whether you're a seasoned entrepreneur or just starting your journey, Founder's Friday is your launchpad for new ideas, valuable connections, and game-changing opportunities.</p>
-                    <a href="/" className="btn btn-branding mb-4">Register For Our Next Event <img src="/images/doubleArrow.svg" alt="double-Arrow" className="doubleArrow"/></a>
+                    <p className="mb-4">Every last Friday of the month, we bring together the brightest minds in our local tech ecosystem. Whether you're a seasoned entrepreneur or just starting your journey, Founder's Friday is your launchpad for new ideas, valuable connections, and game-changing opportunities.</p>
+                    <a href="/" className="btn btn-branding mb-3">Register For Our Next Event <img src="/images/doubleArrow.svg" alt="double-Arrow" className="doubleArrow"/></a>
                     <h6 >Join Us for our next meetup on the 26th of July 2024</h6>
                     <img src="/images/icons.svg" alt="brand-visibility" className="icons" />
                     <div className="callToAction mt-4">Become a collaborator today</div>
                 </div>
                 </div>
+                <div className="col d-none d-lg-block firstP-section-pic">
+                  <img src="images/sectionOnePic.png" alt="pics" className="img-fluid"/>
+                </div>
                 
                 <div></div>
+                <div className="row">
                 <div className="col-md-6">
                     <div className="secondP-section">
-                    <img src= "/images/sectionTwoPic.png" alt="groupPicture" className="img-fluid"/>
+                    <img src= "/images/sectionTwoPic.png" alt="groupPicture" className="img-fluid d-none d-md-block"/>
                     </div>
                 </div>
-                <div className="col-md-6 aboutFounder d-flex justify-content-center p-4">
-                    <div ><img src= "/images/lineOne.png" alt="line" className="line mt-2" />
+                <div className="col-md-6 aboutFounder d-flex justify-content-between p-4">
+                    <div ><img src= "/images/lineOne.png" alt="line" className="line d-none d-lg-block mt-2" />
                     </div>
                     <div>
                     <h3>Who Are We?</h3>
-                    <p className=" ">Born from the vibrant startup ecosystem of Abuja, we recognized the need for a consistent, high-quality networking platform where founders, innovators, and tech enthusiasts could connect, share ideas, and foster collaboration.</p>
+                    <p >Born from the vibrant startup ecosystem of Abuja, we recognized the need for a consistent, high-quality networking platform where founders, innovators, and tech enthusiasts could connect, share ideas, and foster collaboration.</p>
                     <div className="btns">
                         <a href="/" className="btn register-btn">Register <i class="fa-solid fa-arrow-right"></i></a>
                         <a href="http://"  className="btn donate-btn" target="_blank" rel="noopener noreferrer">Donate</a>
                     </div>
-                    <div className="firstLastPara">Founder's Friday is more than just a meetup — it's a movement.</div>
+                    <div className="firstLastPara d-none d-lg-block">Founder's Friday is more than just a meetup — it's a movement.</div>
+                    </div>
                     </div>
                 </div>
             </div>
@@ -80,14 +85,14 @@ export default function(Welcome) {
        </div>
        <div className="fourthPage row">
         
-            <div className="col-md-6">
+            <div className="col-6">
                 <h2>Founders Friday is coming to</h2>
                 <Animated />
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac rutrum felis. Nulla nibh lorem, facilisis vel est at, vehicula dignissim lacus. Cras sagittis imperdiet dolor, eget accumsan leo suscipit id. Maecenas ut ante quis quam lobortis consequat eu id turpis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </p>
                 <a href="http://" target="_blank" rel="noopener noreferrer" className="btn reg-btn">Register <i class="fa-solid fa-arrow-right"></i></a>
             </div>
-            <div className="col-md-6">
-                <img src="/images/small-logo.png" alt="logo" className="img-fluid" />
+            <div className="col-6">
+                <img src="/images/small-logo.png" alt="logo" className="img-fluid d-none d-lg-block " />
            
             </div>
             </div>
@@ -162,7 +167,7 @@ export default function(Welcome) {
 </div>
                 
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 d-none d-lg-block ">
                 <div class="accordion accordion-flush" id="accordionFlushExample">
   <div class="accordion-item">
     <h2 class="accordion-header">
@@ -210,7 +215,7 @@ export default function(Welcome) {
               </div>
               <hr />
               <div className="footer-links">
-                <ul className="social-links ">
+                <ul className="social-links d-none d-lg-block ">
                     <li><a href="/"><i class="fa-brands fa-facebook"></i></a></li>
                     <li><a href="/"><i class="fa-brands fa-instagram"></i></a></li>
                     <li><a href="/"><i class="fa-brands fa-twitter"></i></a></li>
